fc29fae05e4e19fe44bdcb928e638830a66d197d
atafatta <atafatta@gmail.com>
2022-11-15 18:06:58 +0700

    menambah border pada header index html

--- src/main/java/idcard/takehomeidcard/controller.java
+++ src/main/java/idcard/takehomeidcard/controller.java
@@ -4,10 +4,39 @@
  */
 package idcard.takehomeidcard;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
 /**
  *
  * @author STRIX
  */
+@Controller
 public class controller {
-    
+    @ResponseBody
+    @RequestMapping("/getdata")
+    public String getData(@RequestParam("nama")String text,
+                          @RequestParam("image") MultipartFile file,
+                          @RequestParam("tgl")@DateTimeFormat(pattern = "yyyy-MM-dd")Date date)
+                          throws IOException{
+
+//          Date date = new Date();
+//          SimpleDateFormat myDate = new SimpleDateFormat("dd/MM/yyyy");
+//          tanggal = myDate.format(date);
+            SimpleDateFormat tanggal = new SimpleDateFormat("dd/EE/MMMM/yyyy");
+            
+            String newTanggal = tanggal.format(date);
+            
+            String blob = Base64.encodeBase64String(file.getBytes());
+          
+        return text+"<br><h1> ini gambar</h1>"+"<img src='data:image/jpeg;base64,"+blob+"'/><br>"+newTanggal;    
+    }
 }
