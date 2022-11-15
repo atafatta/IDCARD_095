4c28b5cf8ae740c5eae06f16b6a159f8503ae8bd
atafatta <atafatta@gmail.com>
2022-11-15 16:44:30 +0700

    import zip start spring io

---
+++ src/main/java/idcard/takehomeidcard/TakehomeidcardApplication.java
@@ -0,0 +1,13 @@
+package idcard.takehomeidcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakehomeidcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakehomeidcardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/idcard/takehomeidcard/TakehomeidcardApplicationTests.java
@@ -0,0 +1,13 @@
+package idcard.takehomeidcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakehomeidcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
